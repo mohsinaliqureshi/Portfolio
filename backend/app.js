f59172579025cea75a96f96df6e02729a7103088
Mohisn Qureshi <mohsinaliqureshi098@gmail.com>
2024-11-13 16:50:49 +0500

    Update backend/app.js and config.env

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv"
+import cors from "cors"
 
 const app = express();
 
 dotenv.config({path: "./config/config.env"});
 console.log(process.env.PORT)
-
+// FOR CONNECTING TO FRONTEND
+app.use(cors({
+    origin:[process.env.PORTFOLIO_URL,process.env.DASHBOARD__URL],
+    methods:["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+}))
 export default app;
